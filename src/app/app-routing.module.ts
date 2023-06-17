a146a1f95f03e6f2ed2c61e84723692a1567f73d
Martin Bravo <martin.bravo1@danone.com>
2023-06-17 12:14:48 -0600

    Creando estructura de la app con navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DailyCaloriesComponent } from './components/daily-calories/daily-calories.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'daily-calories', component: DailyCaloriesComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DailyCaloriesComponent } from './components/daily-calories/daily-calories.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DailyCaloriesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
